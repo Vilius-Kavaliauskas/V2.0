46a9e78b40dcd8164b6ed59e08a2d44f45fd1286
Vilius-Kavaliauskas <90034722+Vilius-Kavaliauskas@users.noreply.github.com>
2021-11-18 13:04:08 +0200

    Delete v0.4 directory

--- v0.4/Balas.h
+++
@@ -1,6 +0,0 @@
-#ifndef BALAS_H
-#define BALAS_H
-
-void balas(std::string tipas, std::vector <studentas>& stud);
-
-#endif
--- v0.4/Input.h
+++
@@ -1,6 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-void input(std::vector <studentas>& stu, std::string ras);
-
-#endif
